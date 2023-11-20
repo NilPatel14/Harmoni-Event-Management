85f2b3c4efff2d7416e8e81abf1cd29ce0242f43
NilPatel14 <pnil7877@gmail.com>
2023-11-20 10:50:15 +0530

    Add inline tabular

--- event_data/admin.py
+++ event_data/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from event_data.models import *
 
+class Event_workhands(admin.TabularInline):
+    model = Event_workhand
+
+class Event_admin(admin.ModelAdmin):
+    inlines = [Event_workhands]
 
 admin.site.register(profile)
 admin.site.register(Workhand)
 admin.site.register(Company)
-admin.site.register(Event)
+admin.site.register(Event,Event_admin)
 admin.site.register(Event_workhand)
 admin.site.register(Event_Registrations)
 admin.site.register(Workhand_category)
