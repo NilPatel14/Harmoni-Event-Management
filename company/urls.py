1a287685463ad8c6e2e769d0e79cb156ddb62dab
NilPatel14 <pnil7877@gmail.com>
2024-01-02 23:11:20 +0530

    Request Approve Page Done

--- company/urls.py
+++ company/urls.py
@@ -5,6 +5,8 @@ from EventManagement import urls
 urlpatterns = [
     path("add-event/" , views.add_event , name="addevent"),
     path("get-subcat/",views.get_subcat , name = "get_subcat"),
+    path("workhands-requests/",views.workhand_requests,name="workhand_requests"),
+    path("request-approve/",views.request_approve,name="request_approve"),
     path("payment/",views.payment , name="payment"),
     path('payment/success/',views.success,name="success"),
 
