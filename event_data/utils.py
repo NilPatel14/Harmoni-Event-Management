df8695c30340596f1db84903f9bc6dfd94fa5761
NilPatel14 <pnil7877@gmail.com>
2024-02-11 01:43:45 +0530

    Workhand Profile done from company side

--- event_data/utils.py
+++ event_data/utils.py
@@ -19,3 +19,12 @@ def generate_slug_comapny(title:str) -> str:
         title = f'{slugify(title)}-{str(uuid.uuid4())[:4]}'
 
     return title
+
+def generate_slug_workhand(title:str) -> str:
+    from .models import Workhand
+    title = slugify(title)
+
+    while(Workhand.objects.filter(slug = title).exists()):
+        title = f'{slugify(title)}-{str(uuid.uuid4())[:4]}'
+
+    return title
